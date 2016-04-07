7c513580ada96ba984096b1c7b2fa873b7deb344
Vojin Janevski <vojin.janevski@soprex.com>
2016-04-07 16:58:43 +0200

    SI-463 Fixed PSR-2 standard issue.

--- newsletter2go/controllers/front/Export.php
+++ newsletter2go/controllers/front/Export.php
@@ -166,7 +166,8 @@ class Newsletter2GoExportModuleFrontController extends ModuleFrontController
      */
     protected function errorMessage($message = '', $code = 18)
     {
-        return simplexml_load_string('<?xml version="1.0" encoding="UTF-8" ?><prestashop xmlns:xlink="http://www.w3.org/1999/xlink">
+        return simplexml_load_string('<?xml version="1.0" encoding="UTF-8" ?>
+            <prestashop xmlns:xlink="http://www.w3.org/1999/xlink">
             <errors><error><code><![CDATA[ ' . $code . ' ]]></code><message><![CDATA[ ' . $message . ' ]]></message>
             </error></errors></prestashop>');
     }
